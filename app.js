faf6f8664f6e429e80d336579a4c810583829b88
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-30 10:41:02 -0700

    Create index.ejs

--- app.js
+++ app.js
@@ -3,22 +3,18 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-
-    // res.send('<h1>Home Page</h1>');
-
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-
-    // res.send('<h1>About Page</h1>');
-
-    res.sendFile('./views/about.html', { root: __dirname });
-
+    res.render('about');
 });
 
 // redirects 
