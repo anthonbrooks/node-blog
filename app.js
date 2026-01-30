3073820b2acfe9ac521c8e894d4da955646e6d9d
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-30 13:19:40 -0700

    Display blogs on index.ejs

--- app.js
+++ app.js
@@ -12,19 +12,24 @@ app.listen(3000);
 
 // render home
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum Yoshi'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum Mario'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum Bowser'},
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 // render about
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a New Blog' });
 });
 
 // 404 page 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
