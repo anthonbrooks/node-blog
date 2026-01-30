fc13d5320e0f9aecbf544d103b3ac995da7788d1
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-29 17:25:18 -0700

    Write responses to browser

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<h1>Hello, World!</h1>');
+    res.write('<p>This is my first node app.</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
