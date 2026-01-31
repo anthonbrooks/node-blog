96f1b790b288aaf9787b1db3389314910c89416a
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-31 15:58:40 -0800

    Add POST functionality

--- app.js
+++ app.js
@@ -17,38 +17,37 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use((morgan('dev')));
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-// routes
+// render home
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum Yoshi'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum Mario'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum Bowser'},
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 // render about
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort( { createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result  })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
 });
 
 app.get('/blogs/create', (req, res) => {
