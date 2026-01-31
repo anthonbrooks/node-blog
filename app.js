81a3a434e451ab57f9588c2fe3236788965507e9
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-31 11:52:53 -0800

    Import Blog into app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
@@ -14,13 +15,28 @@ mongoose.connect(dbURI)
 // register view engine
 app.set('view engine', 'ejs');
 
-
-
 // middleware & static files
 app.use(express.static('public'));
 app.use((morgan('dev')));
 
-// render home
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+});
+
+// routes
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum Yoshi'},
